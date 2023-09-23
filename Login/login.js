2e26033616485300a99b5c009ce10f67f33e3456
poojabairagi3 <123469728+poojabairagi3@users.noreply.github.com>
2023-09-23 06:03:24 +0530

    Creating Login Screen Frontend
    Creating Login Screen Frontend

---
+++ Login/login.js
@@ -0,0 +1,37 @@
+async function login(e) {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const loginDetails = {
+
+        email: email,
+        password: password
+    }
+   
+    try {
+
+        let response = await axios.post('http://localhost:3000/user/login', loginDetails)
+        if (response.status === 201){
+            alert(response.data.message);
+            console.log(response.data);
+            
+            
+
+        }
+        else if (response.status === 400) {
+            alert(response.data.message);
+        }
+        else if (response.status === 401) {
+            alert(response.data.message);
+        }
+        else {
+            throw new Error(response.data.message)
+        }
+
+       
+    }
+    catch (err) {
+        document.body.innerHTML += `<div style="color:red;">${err} </div>`
+    }
+}
+
